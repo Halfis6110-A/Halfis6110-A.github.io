0821f727b698ada13439d2d723d0fb83f07c19fc
Holmen3055-A <ezequielgamerezequiel2@gmail.com>
2023-07-07 11:12:11 -0400

    + - Fights Dark Mode

---
+++ Archivos/index.js
@@ -0,0 +1,37 @@
+const mainCanvas = document.getElementById("main-canvas");
+const context = mainCanvas.getContext("2d");
+
+let initialX;
+let initialY;
+
+const dibujar = (cursorX, cursorY) => {
+    context.beginPath();
+    context.moveTo(initialX, initialY);
+    context.lineWidth = 5;
+    context.strokeStyle = "#ffffff";
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.lineTo(cursorX, cursorY);
+    context.stroke();
+
+    initialX = cursorX;
+    initialY = cursorY;
+};
+
+const mouseDown = (evt) => {
+    initialX = evt.offsetX;
+    initialY = evt.offsetY;
+    dibujar(initialX, initialY);
+    mainCanvas.addEventListener("mousemove", mouseMoving);
+};
+
+const mouseMoving = (evt) => {
+    dibujar(evt.offsetX, evt.offsetY);
+};
+
+const mouseUp = () => {
+    mainCanvas.removeEventListener("mousemove", mouseMoving);
+};
+
+mainCanvas.addEventListener("mousedown", mouseDown); 
+mainCanvas.addEventListener("mouseup", mouseUp);
